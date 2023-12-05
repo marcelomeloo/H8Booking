e6df7b12dc935309fdb026a55467812777ee78e1
Marcelo Melo <melomarceloliveira@gmail.com>
2023-12-05 17:47:36 -0300

    fix: use body on reservation routes

--- app/api/rooms/[id]/reservations/route.ts
+++ app/api/rooms/[id]/reservations/route.ts
@@ -2,68 +2,80 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { RESERVATION_STATUS } from "@prisma/client";
 
-export async function GET(req: NextRequest, context: { params: { id: number; } }) {
-    const id = context.params.id;
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: number } }
+) {
+  const id = context.params.id;
 
-    if(!id) {
-        return NextResponse.json(
-            { message: "Internal Server Error" },
-            { status: 500 }
-        )
-    }
+  if (!id) {
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 
-    const reservations = await prisma.reservations.findMany({
-        where: {
-            roomId: id, 
-            status: RESERVATION_STATUS.APPROVED
-        }
-    })
+  const reservations = await prisma.reservations.findMany({
+    where: {
+      roomId: id,
+      status: RESERVATION_STATUS.APPROVED,
+    },
+  });
 
-    return NextResponse.json( { reservations }, { status: 200})
+  return NextResponse.json({ reservations }, { status: 200 });
 }
 
-export async function POST(req: NextRequest, context: { params: { id: number; } }) {
-    const roomId = context.params.id;
-    const { searchParams } = req.nextUrl;
-    const userId = searchParams.get("userId");
-    const startDate = searchParams.get("startDate");
-    const endDate = searchParams.get("endDate");
+export async function POST(
+  req: NextRequest,
+  context: { params: { id: number } }
+) {
+  const roomId = context.params.id;
+  const body = await req.json();
 
-    if(!roomId || !userId || !startDate || !endDate) {
-        return NextResponse.json(
-            { message: "Internal Server Error" },
-            { status: 500 }
-        )
-    }
+  const userId = body.userId;
+  const startDate = body.startDate;
+  const endDate = body.endDate;
 
-    const reservation = await prisma.reservations.create({
-        data: {
-            roomId: parseInt(roomId.toString()),
-            userId: parseInt(userId),
-            status: RESERVATION_STATUS.APPROVED,
-            init_time: new Date(startDate),
-            end_time: new Date(endDate)
-        }
-    })
+  if (!roomId || !userId || !startDate || !endDate) {
+    return NextResponse.json(
+      { message: "Missing important information" },
+      { status: 422 }
+    );
+  }
 
-    return NextResponse.json( { reservation }, { status: 200})
+  const reservation = await prisma.reservations.create({
+    data: {
+      roomId: parseInt(roomId.toString()),
+      userId: parseInt(userId),
+      status: RESERVATION_STATUS.APPROVED,
+      init_time: new Date(startDate),
+      end_time: new Date(endDate),
+    },
+  });
+
+  return NextResponse.json({ reservation }, { status: 200 });
 }
 
-export async function PATCH(req: NextRequest, context: {params: {id: number} } ) {
-    const id = context.params.id;
-    const reservationId = req.body.id;
+export async function PATCH(
+  req: NextRequest,
+  context: { params: { id: number } }
+) {
+  const roomId = context.params.id;
+  const body = await req.json();
 
-    if(!id || !reservation_id) {
-        return NextResponse.json(
-            { message: "Internal Server Error" },
-            { status: 500 }
-        )
-    }
+  const reservationId = body.id;
 
-    const reservation = await prisma.reservations.update({
-        where: { id: Number(reservationId) },
-        data: { status: status }
-    })
+  if (!roomId || !reservationId) {
+    return NextResponse.json(
+      { message: "Missing important information" },
+      { status: 422 }
+    );
+  }
 
-    return NextResponse.json( { reservation }, { status: 200})
+  const reservation = await prisma.reservations.update({
+    where: { id: Number(reservationId) },
+    data: { status: RESERVATION_STATUS.CANCELED },
+  });
+
+  return NextResponse.json({ reservation }, { status: 200 });
 }
