c2680ffe80b2d20562509c1b3486245636b8e08c
marcelversiani <marcelversiani25@gmail.com>
2023-12-06 08:25:08 -0300

    refactor: card component

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -13,6 +13,7 @@ type AvailableRooms = {
 export default async function Rooms() {
   const session = await getServerSession();
   const email = session?.user?.email;
+
   const availableRooms = await getAllRooms(email ?? '');
 
   return (
@@ -20,10 +21,14 @@ export default async function Rooms() {
       <div className="flex flex-row space-x-4">
         {availableRooms.map((room: AvailableRooms) => (
           <Link key={room.id} href={`/rooms/${room.id}/reservations`}>
-            <div className="flex-shrink-0 w-64 p-4 bg-white border rounded-md hover:shadow-md transition duration-300">
-              <div>
-                <h2 className="text-lg font-semibold mb-2">{room.name}</h2>
-                <p className="text-gray-600">{room.description}</p>
+            <div className="flex-shrink-0 w-64 h-60 min-h-60 p-4 bg-white border rounded-md hover:shadow-md transition duration-300">
+              <div className="flex flex-col h-full justify-between">
+                <div>
+                  <h2 className="text-lg font-semibold mb-2 text-center">{room.name}</h2>
+                </div>
+                <div className="flex items-center justify-center h-full">
+                  <p className="text-gray-600 text-center">{room.description}</p>
+                </div>
               </div>
             </div>
           </Link>
